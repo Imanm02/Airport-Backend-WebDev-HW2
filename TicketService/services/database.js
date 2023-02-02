1d630eb1cfaff0e4fcf4ec7874aa968232969234
mynux <arashyadegari0402@gmail.com>
2023-02-02 11:49:40 +0330

    add connection test for db

--- TicketService/services/database.js
+++ TicketService/services/database.js
@@ -4,7 +4,7 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    host: process.env.PG_USER,
+    host: process.env.PG_HOST,
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
@@ -14,6 +14,15 @@ const pool = new Pool({
 pool.on('error', (err) => {
     console.error('Unexpected error on idle client', err)
     process.exit(-1)
+});
+
+// test connection
+pool.query('SELECT NOW()', (err) => {
+//     if err config wrong
+    if (err) {
+        console.log(err);
+        process.exit(-1);
+    }
 });
 
 module.exports = pool;
