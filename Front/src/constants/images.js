82c45206bf0641b06f55736127a06b31bf853b0d
halfblood <arashyadegari0402@gmail.com>
2023-02-18 12:04:52 +0330

    export images

---
+++ Front/src/constants/images.js
@@ -0,0 +1,21 @@
+import logo from '../assets/pics/ticket.png';
+import airport from '../assets/pics/airport.svg';
+import travelTogether from '../assets/pics/travel_together.svg';
+import travelBooking from '../assets/pics/travel_booking.svg';
+import notFound from '../assets/pics/not_found.svg';
+import aseman from '../assets/pics/logos/Aseman.png';
+import kishAir from '../assets/pics/logos/Kish_Air.png';
+import saha from '../assets/pics/logos/Saha.png';
+import zagros from '../assets/pics/logos/Zagros.png';
+
+export default {
+    logo,
+    airport,
+    travelTogether,
+    travelBooking,
+    notFound,
+    aseman,
+    kishAir,
+    saha,
+    zagros,
+}
